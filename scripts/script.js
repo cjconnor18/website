5e7b3d73038f481cc50f8fa8b7874cda7a5eb516
Carol <cjallen1988@gmail.com>
2019-04-28 18:13:09 -0500

    done

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,14 @@ document.getElementById("myHead").innerHTML =
   /*Date Formulas*/
 var date = new Date();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
 document.getElementById("myNav").innerHTML =
   "<ul>"
     + "<li class='dateNav'>" + months[date.getMonth()] +" "+ date.getDate() +", "+ date.getFullYear() +"<span id='dateNavMonth'></span> <span id='dateNavDay'></span> <span id='dateNavYear'></span></li>"
     + "<li><a href='index.html'>Home</a></li>"
-    + "<li><a onclick='eventsFunction()' id='eventsButton' class='eventsButton' href='#'>Events</a>"
+    + "<li><a onclick="eventsFunction() +"id='eventsButton' class='eventsButton' href='#'>Events</a>"
       + "<ul id='eventsDropMenu' class='eventsDropMenu'>"
         + "<li><a href='pages/passwordgrad.html'>Graduation</a></li>"
         + "<li><a href='pages/passwordrev.html'>Reveal</a></li>"
