0a4e5011e5b7d4265dfc99830ae0945a72fd66b8
Carol <cjallen1988@gmail.com>
2019-04-28 18:53:21 -0500

    relative folders

--- scripts/headerFooter.js
+++ scripts/headerFooter.js
@@ -10,16 +10,16 @@ var months = ["January", "February", "March", "April", "May", "June", "July", "A
 document.getElementById("myNav").innerHTML =
   "<ul>"
     + "<li class='dateNav'>" + months[date.getMonth()] +" "+ date.getDate() +", "+ date.getFullYear() +"<span id='dateNavMonth'></span> <span id='dateNavDay'></span> <span id='dateNavYear'></span></li>"
-    + "<li><a href='index.html'>Home</a></li>"
+    + "<li><a href='/index.html'>Home</a></li>"
     + "<li><a onclick=eventsFunction() id='eventsButton' class='eventsButton' href='#'>Events</a>"
       + "<ul id='eventsDropMenu' class='eventsDropMenu'>"
-        + "<li><a href='pages/passwordgrad.html'>Graduation</a></li>"
-        + "<li><a href='pages/passwordrev.html'>Reveal</a></li>"
-        + "<!--<li><a href='pages/another.html'>Another</a></li>-->"
+        + "<li><a href='/pages/passwordgrad.html'>Graduation</a></li>"
+        + "<li><a href='/pages/passwordrev.html'>Reveal</a></li>"
+        + "<!--<li><a href='/pages/another.html'>Another</a></li>-->"
       + "</ul>"
     + "</li>"
-    + "<li><a href='about.html'>About</a></li>"
-    + "<li class='contact'><a href='contact'>Contact</a></li>"
+    + "<li><a href='/pages/about.html'>About</a></li>"
+    + "<li class='/pages/contact'><a href='contact'>Contact</a></li>"
   + "</ul>";
 document.getElementById("myFooter").innerHTML =
 	"<p>23Connor &copy; " + new Date().getFullYear() + "</p>";
