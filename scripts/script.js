0edd5a5821103fbfed598ab30f773d0c3ca10e7e
Carol <cjallen1988@gmail.com>
2019-04-28 17:45:45 -0500

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
 document.getElementById("myHead").innerHTML =
 	"<h1>23Connor</h1>"
 document.getElementById("myNav").innerHTML =
-	"<ul id='navLinks'>"
-	+ "<li><a href='index.html'>Home</a></li>"
-	+ "<li><a href='about.html'>About</a>"
-	+ "<li><a href='donate.html'>Donate</a></li>"
-	+ "</ul>";
+  "<ul>"
+    + "<li class="dateNav"><span id="dateNavMonth"></span> <span id="dateNavDay"></span> <span id="dateNavYear"></span></li>"
+    + "<li><a href="index.html">Home</a></li>"
+    + "<li><a onclick="eventsFunction()" id="eventsButton" class="eventsButton" href="#">Events</a>"
+      + "<ul id="eventsDropMenu" class="eventsDropMenu">"
+        + "<li><a href="pages/passwordgrad.html">Graduation</a></li>"
+        + "<li><a href="pages/passwordrev.html">Reveal</a></li>"
+        + "<!--<li><a href="pages/another.html">Another</a></li>-->"
+      + "</ul>"
+    + "</li>"
+    + "<li><a href="about.html">About</a></li>"
+    + "<li class="contact"><a href="contact">Contact</a></li>"
+  + "</ul>";
 document.getElementById("myFooter").innerHTML =
 	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
 	+ " rights reserved.</p>"
