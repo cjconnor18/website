f6c7db98fed51f7ca104e6ba4241a4fb6eddf98c
Carol <cjallen1988@gmail.com>
2019-04-28 17:34:20 -0500

    javasc

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+document.getElementById("myHead").innerHTML =
+	"<h1>23Connor</h1>"
+document.getElementById("myNav").innerHTML =
+	"<ul id='navLinks'>"
+	+ "<li><a href='index.html'>Home</a></li>"
+	+ "<li><a href='about.html'>About</a>"
+	+ "<li><a href='donate.html'>Donate</a></li>"
+	+ "</ul>";
+document.getElementById("myFooter").innerHTML =
+	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
+	+ " rights reserved.</p>"
+	+ "<p id='credits'>Layout by You</p>"
+	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
+	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
 /*Functions for navigation bar menus*/
 var x = document.getElementById("eventsDropMenu");
 function eventsFunction(){
