d8c38b7eaf25079ec575d4a627d05788ab420901
Carol <cjallen1988@gmail.com>
2019-06-20 20:40:34 -0500

    s

--- scripts/babyshower.js
+++ scripts/babyshower.js
@@ -1,6 +1,15 @@
+var facts1 = "Color of baby's eyes at birth";
+var facts2 = "Babies are born without this part of the leg";
+var facts3 = "First sense that a baby develops";
+var facts4 = "Most babies are born in this month";
+var facts5 = "Babies are born with this many bones";
+
+
+
+
 function facts1(obj){
   obj.style.backgroundColor = "#1ec5e5";
-  obj.innerHTML = "Release Me";
+  obj.innerHTML = obj;
 }
 function facts1End(obj){
   obj.style.backgroundColor = "#00f";
