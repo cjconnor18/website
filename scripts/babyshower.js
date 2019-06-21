4530c8809cd6e69898a567f266362c2275f99ec5
Carol <cjallen1988@gmail.com>
2019-06-20 20:45:01 -0500

    s

--- scripts/babyshower.js
+++ scripts/babyshower.js
@@ -1,16 +1,40 @@
-var facts1 = "Color of baby's eyes at birth";
-var facts2 = "Babies are born without this part of the leg";
-var facts3 = "First sense that a baby develops";
-var facts4 = "Most babies are born in this month";
-var facts5 = "Babies are born with this many bones";
+var facts1Q = "Color of baby's eyes at birth";
+var facts2Q = "Babies are born without this part of the leg";
+var facts3Q = "First sense that a baby develops";
+var facts4Q = "Most babies are born in this month";
+var facts5Q = "Babies are born with this many bones";
 
 
 
 
 function facts1(obj){
-  obj.style.backgroundColor = "#1ec5e5";
-  obj.innerHTML = obj;
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = facts1Q;
 }
+function facts2(obj){
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = facts2Q;
+}
+function facts3(obj){
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = facts3Q;
+}
+function facts4(obj){
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = facts4Q;
+}
+function facts5(obj){
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = facts5Q;
+}
+
+
+
+
+
+
+
+
 function facts1End(obj){
   obj.style.backgroundColor = "#00f";
   obj.innerHTML = "";
