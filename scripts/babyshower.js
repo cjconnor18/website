26568925c0f6afcdcaab234cab8e9a00dfbbabb0
Carol <cjallen1988@gmail.com>
2019-06-20 20:29:16 -0500

    s

--- scripts/babyshower.js
+++ scripts/babyshower.js
@@ -1,1 +1,8 @@
-
+function facts1(obj){
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "Release Me";
+}
+function facts1End(){
+  obj.style.backgroundColor = "#00f";
+  obj.innerHTML = "";
+}
