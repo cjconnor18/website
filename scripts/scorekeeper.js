285860724e9f56fea11dffb688e87831ac300629
Carol <cjallen1988@gmail.com>
2019-07-10 18:59:34 -0500

    test js

---
+++ scripts/scorekeeper.js
@@ -0,0 +1,13 @@
+var score1=document.getElementsByClassName('score1');
+var score2=document.getElementsByClassName('score2');
+var x;
+
+var score1x=0;
+var score2x=0;
+
+function scoreUp(x){
+  if(x==team1){
+    score1x++;
+    score1.innerHTML=score1x;
+  }
+}
